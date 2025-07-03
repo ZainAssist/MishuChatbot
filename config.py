ddf70e6ecce412faf48f5f4a2e59aaefabca41a6
Ȥ⋏ιꪀ <158439852+ZainAssist@users.noreply.github.com>
2025-07-03 06:21:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
-import os
+from os import getenv
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
-LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID", "0"))
-MONGO_URI = os.getenv("MONGO_URI")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "26467271"
+# -------------------------------------------------------------
+API_HASH = "cdb49c92e3d8f2e51152c813dcfe15be"
+# --------------------------------------------------------------
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+STRING1 = getenv("STRING_SESSION", None)
+MONGO_URL = getenv("MONGO_URL", None)
+OWNER_ID = int(getenv("OWNER_ID", "7860277015"))
+SUPPORT_GRP = "MusicOnMasti"
+UPDATE_CHNL = "About_Zain"
+OWNER_USERNAME = "Uff_Zainu"
