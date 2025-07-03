90bb8c5f735d525a54df0fe6fcd24f53ce9cecf7
Ȥ⋏ιꪀ <158439852+ZainAssist@users.noreply.github.com>
2025-07-03 06:10:37 +0530

    Delete handlers directory

--- handlers/voice.py
+++
@@ -1,7 +0,0 @@
-from aiogram import types, Dispatcher
-
-async def handle_voice(message: types.Message):
-    await message.reply("I received your voice 🎤 but I'm still learning to understand audio!")
-
-def register_voice_handlers(dp: Dispatcher):
-    dp.register_message_handler(handle_voice, content_types=types.ContentType.VOICE)
